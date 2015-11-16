12c27c290398c51e95a58ac73296e5eb213782fb
Tradable ApS <Tradable1@Tradables-Mac-mini.local>
2015-11-16 14:27:14 +0100

    initial commit

---
+++ TradableAPI.framework/Headers/TradableAPI.h
@@ -0,0 +1,19 @@
+//
+//  TradableAPI.h
+//  TradableAPI
+//
+//  Created by Tradable ApS on 29/10/15.
+//  Copyright © 2015 Tradable ApS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TradableAPI.
+FOUNDATION_EXPORT double TradableAPIVersionNumber;
+
+//! Project version string for TradableAPI.
+FOUNDATION_EXPORT const unsigned char TradableAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TradableAPI/PublicHeader.h>
+
+
